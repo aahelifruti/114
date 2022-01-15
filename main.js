a5026321f0af2b9b5197f11f11013c1403bc267f
aahelifruti <92666603+aahelifruti@users.noreply.github.com>
2022-01-15 20:48:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX = 0;
+noseY = 0;
+
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 
 }
 
@@ -19,13 +23,16 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].nose.x);
-        console.log("nose y = " + results[0].nose.y);
+        noseX = results[0].pose.nose.x - 12;
+        noseY = results[0].pose.nose.y - 12;
+
+
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot() {
